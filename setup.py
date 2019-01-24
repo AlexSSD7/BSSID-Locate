92df10f6575ba098ab572663a9929fb8c719ae89
Alexander <sashasadovskyi7@gmail.com>
2019-01-24 19:25:36 +0200

    Permissions Fixed

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import shutil
+import os
 
 print("Copying files...")
 shutil.copyfile("bssid-locate", "/bin/bssid-locate")
+print("Finishing...")
+os.system("chmod +x /bin/bssid-locate")
 print("Done.")
